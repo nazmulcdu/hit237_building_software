47132f121b23b9789b6fbdbba2acbcc5c82e3d18
Ayesha Akter <s379450@students.cdu.edu.au>
2025-04-14 21:05:26 +0930

    added comment

--- hit237_project/mango_proj/mango_proj/urls.py
+++ hit237_project/mango_proj/mango_proj/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mango_pests.urls')),  # include your app routes
+    path('', include('mango_pests.urls')),  # include your app routess
 ]
