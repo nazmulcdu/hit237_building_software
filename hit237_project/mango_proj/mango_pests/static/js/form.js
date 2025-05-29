6da2c043a2e1bca4269e35c5cec3996d14c5c2ab
Sohom Sarker <110902277+ArkaSarker@users.noreply.github.com>
2025-05-29 12:37:42 +0930

    Added a seperate js file userform

---
+++ hit237_project/mango_proj/mango_pests/static/js/form.js
@@ -0,0 +1,23 @@
+const step1 = document.getElementById("form-step-1");
+    const step2 = document.getElementById("form-step-2");
+    const step3 = document.getElementById("form-step-3");
+
+    document.getElementById("nextToStep2").addEventListener("click", function () {
+        step1.style.display = "none";
+        step2.style.display = "block";
+    });
+
+    document.getElementById("backToStep1").addEventListener("click", function () {
+        step2.style.display = "none";
+        step1.style.display = "block";
+    });
+
+    document.getElementById("nextToStep3").addEventListener("click", function () {
+        step2.style.display = "none";
+        step3.style.display = "block";
+    });
+
+    document.getElementById("backToStep2").addEventListener("click", function () {
+        step3.style.display = "none";
+        step2.style.display = "block";
+    });
